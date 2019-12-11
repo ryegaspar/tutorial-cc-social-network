344c147db14196a356f441924926ad8e6890b16b
rye gaspar <gaspar.ryan@outlook.com>
2019-12-11 17:36:00 -0600

    11 - auth and guest middleware

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,16 @@
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/signup', 'AuthController@getSignup')->name('auth.signup');
-Route::post('/signup', 'AuthController@postSignup');
-Route::get('/signin', 'AuthController@getSignin')->name('auth.signin');
-Route::post('/signin', 'AuthController@postSignin');
-Route::get('/signout', 'AuthController@getSignout')->name('auth.signout');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/signup', 'AuthController@getSignup')->name('auth.signup');
+    Route::post('/signup', 'AuthController@postSignup');
+    Route::get('/signin', 'AuthController@getSignin')->name('auth.signin');
+    Route::post('/signin', 'AuthController@postSignin');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/signout', 'AuthController@getSignout')->name('auth.signout');
+});
 // test session info
 //Route::get('/alert', function () {
 //    return redirect()->route('home')->with('info', 'You have signed in');

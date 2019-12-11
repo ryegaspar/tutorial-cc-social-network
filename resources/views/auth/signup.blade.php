cbbf06a8c8f43bdd528ac1edac1f34647ff24403
rye gaspar <gaspar.ryan@outlook.com>
2019-12-10 21:23:36 -0600

    convert to bootstrap 3

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -3,45 +3,32 @@
 @section('content')
     <h3>Sign Up</h3>
     <div class="row">
-        <div class="col-xl-6">
+        <div class="col-lg-6">
             <form class="form-vertical" role="form" method="post" action="{{ route('auth.signup') }}">
                 @csrf
-                <div class="form-group">
-                    <label for="email" class="col-form-label">Your email address</label>
-                    <input type="text"
-                           name="email"
-                           class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
-                           id="email"
-                           value="{{ Request::old('email') ?: ''}}"
-                    >
+                <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+                    <label for="email" class="control-label">Your email address</label>
+                    <input type="text" name="email" class="form-control" id="email" value="">
                     @if ($errors->has('email'))
-                        <span class="invalid-feedback">{{ $errors->first('email') }}</span>
+                        <span class="help-block">{{ $errors->first('email') }}</span>
                     @endif
                 </div>
-                <div class="form-group">
-                    <label for="username" class="col-form-label">Choose a username</label>
-                    <input type="text"
-                           name="username"
-                           class="form-control {{ $errors->has('username') ? 'is-invalid' : '' }}"
-                           id="username"
-                           value="{{ Request::old('username') ?: ''}}"
-                    >
+                <div class="form-group {{ $errors->has('username') ? 'has-error' : '' }}">
+                    <label for="username" class="control-label">Choose a username</label>
+                    <input type="text" name="username" class="form-control" id="username" value="">
                     @if ($errors->has('username'))
-                        <span class="invalid-feedback">{{ $errors->first('username') }}</span>
+                        <span class="help-block">{{ $errors->first('username') }}</span>
                     @endif
                 </div>
-                <div class="form-group">
-                    <label for="password" class="col-form-label">Choose a password</label>
-                    <input type="password"
-                           name="password"
-                           class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
-                           id="password">
+                <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
+                    <label for="password" class="control-label">Choose a password</label>
+                    <input type="password" name="password" class="form-control" id="password">
                     @if ($errors->has('password'))
-                        <span class="invalid-feedback">{{ $errors->first('password') }}</span>
+                        <span class="help-block">{{ $errors->first('password') }}</span>
                     @endif
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-secondary">Sign up</button>
+                    <button type="submit" class="btn btn-default">Sign up</button>
                 </div>
             </form>
         </div>

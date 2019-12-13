95223d2a33d9aa5392ada507f24c58a9ed6509d1
rye gaspar <gaspar.ryan@outlook.com>
2019-12-13 12:49:42 -0600

    28 - showing status replies

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -48,20 +48,29 @@
                                 <li>10 likes</li>
                             </ul>
 
-                            {{--                            <div class="media">--}}
-                            {{--                                <a class="pull-left" href="#">--}}
-                            {{--                                    <img class="media-object" alt="" src="">--}}
-                            {{--                                </a>--}}
-                            {{--                                <div class="media-body">--}}
-                            {{--                                    <h5 class="media-heading"><a href="#">Billy</a></h5>--}}
-                            {{--                                    <p>Yes, it is lovely!</p>--}}
-                            {{--                                    <ul class="list-inline">--}}
-                            {{--                                        <li>8 minutes ago.</li>--}}
-                            {{--                                        <li><a href="#">Like</a></li>--}}
-                            {{--                                        <li>4 likes</li>--}}
-                            {{--                                    </ul>--}}
-                            {{--                                </div>--}}
-                            {{--                            </div>--}}
+                            @foreach($status->replies as $reply)
+                                <div class="media">
+                                    <a class="pull-left" href="{{ route('profile.index', ['username' => $reply->user->username]) }}">
+                                        <img class="media-object"
+                                             alt="{{ $reply->user->getNameOrUsername() }}"
+                                             src="{{ $reply->user->getAvatarUrl() }}">
+                                    </a>
+                                    <div class="media-body">
+                                        <h5 class="media-heading">
+                                            <a href="{{ route('profile.index', ['username' => $reply->user->username]) }}"
+                                            >
+                                                {{ $reply->user->getNameOrUsername() }}
+                                            </a>
+                                        </h5>
+                                        <p>{{ $reply->body }}</p>
+                                        <ul class="list-inline">
+                                            <li>{{ $reply->created_at->diffForHumans() }}</li>
+                                            <li><a href="#">Like</a></li>
+                                            <li>4 likes</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            @endforeach
 
                             <form role="form"
                                   action="{{ route('status.reply', ['statusId' => $status->id]) }}"
